acc3f88d3bb31360168e3421374c796934f965b8
Idktbh12z <108253748+Idktbh12z@users.noreply.github.com>
2023-07-18 13:59:26 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,14 @@
+repeat task.wait() until game:IsLoaded()
+
+if game:GetService("UserInputService").TouchEnabled == true then
+    game.Players.LocalPlayer:Kick("Sorry, no support for mobile!")
+end
 if game.PlaceId == 12389327869 then -- A-888
-  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Idktbh12z/cheapmilk/main/Musc-Fixed-A-888/Main1.lua"))();
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Idktbh12z/cheapmilk/main/Musc-Fixed-A-888/Main1.lua"))()
 elseif game.PlaceId == 8770868695 then -- AA
-  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Idktbh12z/cheapmilk/main/AA/Main.lua"))();
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Idktbh12z/cheapmilk/main/AA/Main.lua"))()
 elseif game.PlaceId == 6735572261 then -- Pilgrammed
-  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Idktbh12z/cheapmilk/main/Pilgrammed/Backup.lua"))();
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Idktbh12z/cheapmilk/main/Pilgrammed/Backup.lua"))()
 else
     return
 end
